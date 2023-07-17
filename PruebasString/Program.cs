37ff9f8cc074ce7721f3bb7113a7c14bb96f3955
Vicente Estrada Dominguez <vicent-e@ra.mx>
2023-07-17 08:28:11 -0600

    Test sin framework

---
+++ PruebasString/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace PruebasString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //PruebaQueStringHolaTieneLogitud4();
+            PruebaQueStringHolaContieneCaracterA();
+            Console.WriteLine("Pruebas Exitosas");
+        }
+
+        static void PruebaQueStringHolaTieneLogitud4()
+        {
+            //Arrange
+            var stringHola = "Hola";
+
+            //Act
+            var longitud = stringHola.Length;
+
+            //Assert
+            if (longitud != 4)
+            {
+                throw new Exception("Prueba fallida");
+            }
+        }
+
+        static void PruebaQueStringHolaContieneCaracterA()
+        {
+            //Arrange
+            var stringHola = "Hola";
+
+            //Act
+            var contieneLetraA = stringHola.Contains("a");
+
+            //Assert
+            if (!contieneLetraA)
+            {
+                throw new Exception("Prueba fallida");
+            }
+        }
+    }
+}
